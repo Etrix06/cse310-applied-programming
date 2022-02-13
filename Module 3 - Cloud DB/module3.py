c161be49ad4a7da64bd29dbcc79928f97474ee81
Etrix06 <alc18005@byui.edu>
2022-02-12 18:28:00 -0700

    module 3 and gdfunctions

--- Module 3 - Cloud DB/module3.py
+++ Module 3 - Cloud DB/module3.py
@@ -1,3 +1,18 @@
 import pymongo
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+MONGO = os.getenv("MONGO")
+
+cluster = MongoClient(f"{MONGO}")
+db = cluster["python"]
+collection = db["python"]
+
+post = {"_id": 0, "name": "Erik", "age": 41}
+
+collection.insert_one(post)
+
+print("Post created in MongoDB.")
