94bf5325cd7374ddf26432555da0d6046b8f5236
Etrix06 <alc18005@byui.edu>
2022-03-03 22:07:11 -0700

    continued

--- Module 4 - SQL DB/module4.py
+++ Module 4 - SQL DB/module4.py
@@ -18,11 +18,12 @@ cursor = connection.cursor()
 
 # create item in database
 #cursor.execute("INSERT INTO food_storage VALUES ('Whole Wheat', 'Distribution Center', '1/15/2017', '4/1/2026', '200', '8' )")
+#cursor.execute("INSERT INTO food_storage VALUES ('Potato Gems', 'Walmart', '2/3/2019', '4/1/2026', '200', '8' )")
 
 # Query DB with Select
-cursor.execute("SELECT * FROM food_storage WHERE name='Whole Wheat'")
+cursor.execute("SELECT * FROM food_storage")
 
-print(cursor.fetchone())   # or cursor.fetchmany()  or cursor.fetchall()
+print(cursor.fetchmany(5))   # or cursor.fetchmany()  or cursor.fetchall()
 
 connection.commit()
 
