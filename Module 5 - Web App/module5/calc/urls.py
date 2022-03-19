ccc45d3789d63f2274c0715c66fd5ecf9ff10069
Etrix06 <alc18005@byui.edu>
2022-03-19 14:23:49 -0700

    updated module 5

--- Module 5 - Web App/module5/calc/urls.py
+++ Module 5 - Web App/module5/calc/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add', views.add, name='add')
+    path('add_info', views.add_info, name='add_info')
 ]
--- Module 5 - Web App/module5/calc/views.py
+++ Module 5 - Web App/module5/calc/views.py
@@ -4,11 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Erik'})
+    return render(request, 'home.html', {'name': 'Player'})   # The plan is to change player to actual name dynamically
 
-def add(request):
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
-    res = val1 + val2
-
-    return render(request, 'result.html', {'result': res})
+def add_info(request):
+    val1 = request.GET['name']
+    val2 = request.GET['campaign']
+    val3 = request.GET['date']
+    res = val1
+    res2 = val2
+    res3 = val3
+    return render(request, 'result.html', {'name': res, 'campaign': res2, 'date': res3})
